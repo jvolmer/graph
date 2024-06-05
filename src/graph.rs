dbb8d3936a1630d0b4aa695f57e245bb9d7fb52e
jvolmer <juliavolmer@t-online.de>
2024-06-05 17:15:34 +0200

    Modify graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,20 +1,39 @@
-struct Graph {
-    vertices: Vec<Vertex>,
+#[derive(Debug, PartialEq)]
+pub struct Graph {
     edges: Vec<Edge>,
 }
-
-struct Vertex {}
-
-struct VertexId {
-    id: usize,
-}
-impl VertexId {
-    pub fn index(&self) -> usize {
-        self.id
+impl Graph {
+    pub fn from(edges: Vec<(usize, usize)>) -> Self {
+        Self {
+            edges: edges
+                .into_iter()
+                .map(|(from, to)| Edge(VertexId(from), VertexId(to)))
+                .collect(),
+        }
     }
 }
 
-struct Edge {
-    from: usize,
-    to: usize,
+#[derive(Debug, PartialEq)]
+pub struct VertexId(pub usize);
+
+#[derive(Debug, PartialEq)]
+struct Edge(VertexId, VertexId);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_graph_from_edge_topology() {
+        assert_eq!(
+            Graph::from(vec![(0, 1), (4, 5), (1, 1)]),
+            Graph {
+                edges: vec![
+                    Edge(VertexId(0), VertexId(1)),
+                    Edge(VertexId(4), VertexId(5)),
+                    Edge(VertexId(1), VertexId(1))
+                ]
+            }
+        );
+    }
 }
